66d1143d0c3458d5495ca70831ba8ef7cf49767e
vimahemo1 <vimahemo@gmail.com>
2026-09-07 16:51:10 -0500

    extraer viewState

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import fs from "fs";
+import * as cheerio from "cheerio";
 const url = "https://pjett.trf5.jus.br/pjeconsulta/ConsultaPublica/listView.seam";
 
 
@@ -12,10 +13,22 @@ async function main() {
             "User-Agent":
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
         }
-    });    
+    });
     console.log("HTTP:", response.status);
     console.log("Tamaño HTML:", response.data.length);
     fs.writeFileSync("response.html", response.data, "utf-8");
     console.log("Archivo response.html guardado.");
+
+    const $ = cheerio.load(
+        response.data
+    );
+    const viewState = $("input[name='javax.faces.ViewState']").attr("value");
+    if (!viewState) {
+        throw new Error("No se encontró el valor de javax.faces.ViewState en la respuesta HTML.");
+    }
+    console.log("Valor de javax.faces.ViewState:", viewState);
 }
-main();
+
+main().catch((error) => {
+    console.error("Error:", error instanceof Error ? error.message : error);
+});
